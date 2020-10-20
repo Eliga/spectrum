396f6020afa82faedf5b8837c1791475ef3d8038
Antony Vitré <antony@eliga.fr>
2020-10-20 19:51:52 +0200

    test

--- spectrum.js
+++ spectrum.js
@@ -3,7 +3,17 @@
 // Author: Brian Grinstead
 // License: MIT
 
-function Spectrum($, undefined) {
+(function (factory) {
+  "use strict";
+
+  if (typeof define === 'function' && define.amd) { // AMD
+    define(['jquery'], factory);
+  } else if (typeof exports == "object" && typeof module == "object") { // CommonJS
+    module.exports = factory(require('jquery'));
+  } else { // Browser
+    factory(jQuery);
+  }
+})(function ($, undefined) {
   "use strict";
 
   var defaultOpts = {
@@ -2459,9 +2469,5 @@ function Spectrum($, undefined) {
       $.fn.spectrum.processNativeColorInputs();
     }
   });
-}
 
-/**
- * @module spectrum
- */
-module.exports = Spectrum;
+});
